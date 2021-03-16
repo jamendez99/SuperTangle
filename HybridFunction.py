b6d465f47474251a16cf6d837ccb13b33d4ede3d
José Alejandro Méndez Méndez <jamm1999@seas.upenn.edu>
2021-03-15 22:42:18 -0400

    Added other helper classes

---
+++ HybridFunction.py
@@ -0,0 +1,44 @@
+class HybridFunction(Function):
+    """ Hybrid quantum - classical function definition """
+    
+    @staticmethod
+    def forward(ctx, input, quantum_circuit, shift):
+        """ Forward pass computation """
+        ctx.shift = shift
+        ctx.quantum_circuit = quantum_circuit
+        
+        expectation_z = ctx.quantum_circuit.run([input[0].tolist()])
+        result = torch.tensor(expectation_z)
+        ctx.save_for_backward(input, result)
+
+        return result
+        
+    @staticmethod
+    def backward(ctx, grad_output):
+        """ Backward pass computation """
+        input, expectation_z = ctx.saved_tensors
+        input_list = np.array(input.tolist())
+        
+        shift_right = input_list + np.ones(input_list.shape) * ctx.shift
+        shift_left = input_list - np.ones(input_list.shape) * ctx.shift
+        
+        gradients = []
+        for i in range(len(input_list)):
+            expectation_right = ctx.quantum_circuit.run([shift_right[i]])
+            expectation_left  = ctx.quantum_circuit.run([shift_left[i]])
+            gradient = torch.tensor(expectation_right) - torch.tensor(expectation_left)
+            gradients.append(gradient)
+        gradients = torch.stack(gradients).transpose(1, 0)
+        return gradients.float() * grad_output.float(), None, None
+
+class Hybrid(nn.Module):
+    """ Hybrid quantum - classical layer definition """
+    
+    def __init__(self, n_qubits, backend, shots, shift):
+        super(Hybrid, self).__init__()
+        self.quantum_circuit = QuantumCircuit(n_qubits, backend, shots)
+        self.shift = shift
+        
+    def forward(self, input):
+        circ_ret = HybridFunction.apply(input, self.quantum_circuit, self.shift)
+        return circ_ret
